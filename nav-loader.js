d24c36e83641651462267b1323314a2733460452
Carlo Ghelfi <ghelfighel@gmail.com>
2026-01-26 18:02:53 +0100

    Add nav-loader.js to load header and footer

---
+++ nav-loader.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Carica Header
+    fetch("header.html").then(response => response.text()).then(data => {
+        document.getElementById("header-placeholder").innerHTML = data;
+    });
+    // Carica Footer
+    fetch("footer.html").then(response => response.text()).then(data => {
+        document.getElementById("footer-placeholder").innerHTML = data;
+    });
+});
